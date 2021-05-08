37b59f59f3276c0f58460064cf56e46c96ccc7f8
cinny123 <82553947+cinny123@users.noreply.github.com>
2021-05-08 21:56:11 +0800

    Add files via upload

---
+++ Stroke_predic_3.py
@@ -0,0 +1,28 @@
+
+
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+data = pd.read_csv('healthcare-dataset-stroke-data.csv')
+charges = data.drop(["id"],axis=1)
+corrDf = charges.apply(lambda x:pd.factorize(x)[0])
+corrDf.head()
+
+# find correlation
+corr=corrDf.corr()
+
+# heat map
+plt.figure(figsize=(10,5),dpi=80)
+ax = sns.heatmap(corr,xticklabels=corr.columns,yticklabels=corr.columns,
+               linewidths=0.2,cmap="YlGnBu",annot=True)
+plt.title("Correlation between variables")
+plt.show()
+
+
+# plot correlation
+factor_data = pd.get_dummies(data.iloc[:,1:])
+plt.figure(figsize=(12,5))
+factor_data.corr()["stroke"].sort_values(ascending=False).plot(kind="bar")
+plt.title("Stroke and variables")
+plt.show()
